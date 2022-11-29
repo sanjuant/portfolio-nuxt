aa861c64dd1c76fd97cd36327cfb013a341d9b0d
Sorrow <sorrow@sanjuant.fr>
2022-11-29 01:57:32 +0100

    refactor: prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,19 +18,40 @@ export default defineNuxtConfig({
       viewport: 'width=device-width, initial-scale=1',
       title: 'Thibaut SANJUAN',
       meta: [
-        { name: 'description', content: 'Je suis ingénieur en Cybersécurité avec une forte expérience en développement.' },
-        { name: 'apple-mobile-web-app-title', content: 'Thibaut SANJUAN - Portfolio' },
+        {
+          name: 'description',
+          content:
+            'Je suis ingénieur en Cybersécurité avec une forte expérience en développement.',
+        },
+        {
+          name: 'apple-mobile-web-app-title',
+          content: 'Thibaut SANJUAN - Portfolio',
+        },
         { name: 'application-name', content: 'Thibaut SANJUAN - Portfolio' },
         { name: 'msapplication-TileColor', content: '#da532c' },
-        { name: 'theme-color', content: '#ffffff' }
+        { name: 'theme-color', content: '#ffffff' },
       ],
       link: [
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
         { rel: 'manifest', href: '/site.webmanifest' },
-        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
-      ]
-    }
-  }
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+      ],
+    },
+  },
 })
