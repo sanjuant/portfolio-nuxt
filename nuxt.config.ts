24dc893ffeb4f0ffba58fe6674c0c046b6e2e2a3
sanjuant <sanjuan.thibaut@gmail.com>
2025-09-12 22:49:29 +0200

    feat(metadata): add metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,68 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from "@tailwindcss/vite";
-import type { PluginOption } from "vite";
+import type {PluginOption} from "vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: {enabled: true},
-  ssr: true,
-  css: ['~/assets/css/swiperjs.css', '~/assets/css/tailwind.css'],
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    ssr: true,
+    css: ['~/assets/css/swiperjs.css', '~/assets/css/tailwind.css'],
 
-  runtimeConfig: {
-      strapiUrl: process.env.NUXT_STRAPI_URL || 'http://portfolio-strapi:1337',
-      strapiApiToken: process.env.NUXT_STRAPI_API_TOKEN || '',
-      public: {
-          strapiUrl: '/api',
-      }
-  },
+    runtimeConfig: {
+        strapiUrl: process.env.NUXT_STRAPI_URL || 'http://portfolio-strapi:1337',
+        strapiApiToken: process.env.NUXT_STRAPI_API_TOKEN || '',
+        public: {
+            strapiUrl: '/api',
+        }
+    },
 
-  vite: {
-      plugins: tailwindcss() as unknown as PluginOption[],
-  },
+    vite: {
+        plugins: tailwindcss() as unknown as PluginOption[],
+    },
 
-  build: {
-      transpile: ['@heroicons/vue'],
-  },
-
+    build: {
+        transpile: ['@heroicons/vue'],
+    },
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            title: 'Thibaut SANJUAN',
+            meta: [
+                {
+                    name: 'description',
+                    content:
+                        'Je suis ingénieur en Cybersécurité avec une forte expérience en développement.',
+                },
+                {
+                    name: 'apple-mobile-web-app-title',
+                    content: 'Thibaut SANJUAN - Portfolio',
+                },
+                {name: 'application-name', content: 'Thibaut SANJUAN - Portfolio'},
+                {name: 'msapplication-TileColor', content: '#da532c'},
+                {name: 'theme-color', content: '#ffffff'},
+            ],
+            link: [
+                {
+                    rel: 'apple-touch-icon',
+                    sizes: '180x180',
+                    href: '/apple-touch-icon.png',
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '32x32',
+                    href: '/favicon-32x32.png',
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '16x16',
+                    href: '/favicon-16x16.png',
+                },
+                {rel: 'manifest', href: '/site.webmanifest'},
+                {rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5'},
+            ],
+        },
+    },
 })
