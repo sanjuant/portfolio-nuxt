005791efe5c80532c331403ba84eb207cecbc01b
sanjuant <sanjuan.thibaut@gmail.com>
2025-09-12 23:23:15 +0200

    fix(metadata): remove unused favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,15 +50,8 @@ export default defineNuxtConfig({
                 },
                 {
                     rel: 'icon',
-                    type: 'image/png',
-                    sizes: '32x32',
-                    href: '/favicon-32x32.png',
-                },
-                {
-                    rel: 'icon',
-                    type: 'image/png',
-                    sizes: '16x16',
-                    href: '/favicon-16x16.png',
+                    type: 'image/x-icon',
+                    href: '/favicon.ico'
                 },
                 {rel: 'manifest', href: '/site.webmanifest'},
                 {rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5'},
